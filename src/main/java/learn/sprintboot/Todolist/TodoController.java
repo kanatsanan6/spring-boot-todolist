b18ad9c6ff38be02199218b967885ae95aa3b8da
Kanatsanan Janpakdee <kanatsanan.j1998@gmail.com>
2022-12-10 11:50:42 +0700

    feat: GET todo endpoint

---
+++ src/main/java/learn/sprintboot/Todolist/TodoController.java
@@ -0,0 +1,25 @@
+package learn.sprintboot.Todolist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/todo")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getTodos() {
+        return todoService.getTodos();
+    }
+}
---
+++ src/main/java/learn/sprintboot/Todolist/TodoService.java
@@ -0,0 +1,20 @@
+package learn.sprintboot.Todolist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+}
